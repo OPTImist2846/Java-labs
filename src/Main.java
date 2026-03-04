310b34e73fa9d3182c7ebaadd8a95dd71dfc44f6
OPTImist2846 <olessolja@gmail.com>
2026-03-05 00:38:49 +0200

    did Task2

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double celsius = 25.0;
+        double fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+
+        System.out.println(celsius + " градусів за Цельсієм дорівнює " + fahrenheit + " градусів за Фаренгейтом.");
+    }
+}
