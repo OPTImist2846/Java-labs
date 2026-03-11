b194432b729f1d4e901f2c01569061d903561dbd
OPTImist2846 <olessolja@gmail.com>
2026-03-11 19:27:09 +0200

    Main: add class InvalidPaymentException + Deckription

--- Laba2 Java/src/Main.java
+++ Laba2 Java/src/Main.java
@@ -1,6 +1,17 @@
-void main() {
-  IO.println(String.format("Hello and welcome!"));
-  for (int i = 1; i <= 5; i++) {
-    IO.println("i = " + i);
+class InvalidPaymentException extends Exception {
+  public InvalidPaymentException(String message) {
+    super(message);
+  }
+}
+
+public class Main {
+
+  public static void processPayment(double amount) throws InvalidPaymentException {
+
+    if (amount <= 0) {
+      throw new InvalidPaymentException("Сума платежу не може бути нульовою або від'ємною! Передано: " + amount);
+    }
+
+    System.out.println("Платіж" + amount + " успішно проведено.");
   }
 }
