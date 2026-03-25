fe15e6080ddf3496d9e877fb2ccddde000cec931
OPTImist2846 <olessolja@gmail.com>
2026-03-25 21:23:02 +0200

    Add verification for 2 and 3 Task

--- Laba3 Java/Laba3/src/Main.java
+++ Laba3 Java/Laba3/src/Main.java
@@ -1,28 +1,38 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("=== ТЕСТ ЗАВДАННЯ 1 (Map) ===");
-        StudentRegistry registry = new StudentRegistry();
 
+    public static void main(String[] args) {
+        System.out.println("ЗАПУСК ЛАБОРАТОРНОЇ РОБОТИ №3\n");
+
+        testTask1And2();
+        testTask3And4();
+    }
+
+    private static void testTask1And2() {
+        System.out.println(" ТЕСТ ЗАВДАННЯ 1 (Map)");
+        StudentRegistry registry = new StudentRegistry();
         registry.addStudent(new Student(101, "Олександр"));
         registry.addStudent(new Student(102, "Марія"));
-        registry.addStudent(new Student(103, "Іван"));
-
-        registry.displayAllStudents();
-        registry.findStudent(102);
-        registry.removeStudent(103);
         registry.displayAllStudents();
 
         System.out.println("\nТЕСТ ЗАВДАННЯ 2 (Set та підрахунок)");
-
-        List<String> names = Arrays.asList("Анна", "Петро", "Анна", "Іван", "Петро", "Анна");
+        List<String> names = Arrays.asList("Анна", "Петро", "Анна", "Іван");
         System.out.println("Оригінальний список: " + names);
-
-        Set<String> uniqueNames = UniqueStorage.getUniqueElements(names);
-        System.out.println("Унікальні елементи (Set): " + uniqueNames);
-
-        Map<String, Integer> occurrences = UniqueStorage.countOccurrences(names);
-        System.out.println("Кількість входжень (Map): " + occurrences);
+        System.out.println("Унікальні (Set): " + UniqueStorage.getUniqueElements(names));
+        System.out.println("Кількість (Map): " + UniqueStorage.countOccurrences(names));
     }
+
+    private static void testTask3And4() {
+        System.out.println("ТЕСТ ЗАВДАННЯ 3 (Box<T>)");
+        Box<Integer> integerBox = new Box<>();
+        integerBox.putItem(123);
+        System.out.println("У коробці з числами лежить: " + integerBox.getItem());
+
+        System.out.println("\nТЕСТ ЗАВДАННЯ 4 (findMax)");
+        Integer[] intArray = {3, 9, 1, 14, 5};
+        System.out.println("Максимум у Integer[]: " + GenericUtils.findMax(intArray));
+    }
+
+
 }
