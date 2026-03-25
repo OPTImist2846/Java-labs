f6fd434efdcc5dbd3d4adfa1b94643e49c53e3f2
OPTImist2846 <olessolja@gmail.com>
2026-03-25 21:10:44 +0200

    Add class UniqueStorage

--- Laba3 Java/Laba3/src/UniqueStorage.java
+++ Laba3 Java/Laba3/src/UniqueStorage.java
@@ -1,2 +1,15 @@
+import java.util.*;
+
 public class UniqueStorage {
+    public static <T> Set<T> getUniqueElements(List<T> inputList) {
+        return new HashSet<>(inputList);
+    }
+
+    public static <T> Map<T, Integer> countOccurrences(List<T> inputList) {
+        Map<T, Integer> counts = new HashMap<>();
+        for (T item : inputList) {
+            counts.put(item, counts.getOrDefault(item, 0) + 1);
+        }
+        return counts;
+    }
 }
