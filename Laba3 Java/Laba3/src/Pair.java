8d9c3e95012f61fb21624ee33409755a8c83ea74
OPTImist2846 <olessolja@gmail.com>
2026-03-25 21:30:10 +0200

    Add class  Pair

---
+++ Laba3 Java/Laba3/src/Pair.java
@@ -0,0 +1,22 @@
+public class Pair<T, U> {
+    private T first;
+    private U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() { return first; }
+    public U getSecond() { return second; }
+
+    public boolean comparePairs(Pair<T, U> otherPair) {
+        return this.first.equals(otherPair.getFirst()) &&
+                this.second.equals(otherPair.getSecond());
+    }
+
+    @Override
+    public String toString() {
+        return "Парa [" + first + ", " + second + "]";
+    }
+}
